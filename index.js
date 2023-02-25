7d1b874ad7186182b0946ed895ecc5609249e015
Alejandro Lopez <alexjes6543@gmail.com>
2023-02-25 19:58:21 -0400

    test

--- index.js
+++ index.js
@@ -23,36 +23,36 @@ app.get("/", (req, res) => {
 
 app.post("/login", async (req, res) => {
   try {
-    const { dni, password } = req.body;
-    if (!(dni && password)) {
-      res.status(400).send("All input is required");
-    }
-    let dbquery = `SELECT * FROM users WHERE dni=${dni}`;
-    const oldUser = await db.query(dbquery);
-    if (Array.isArray(oldUser) && oldUser.length === 0) {
-      return res.status(409).send("User Does not  Exist. Please Register");
-    }
-    const user = oldUser[0];
-    if (await bcrypt.compare(password, user.password)) {
-      const token = jwt.sign(
-        { user_id: user.dni, dni },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
-      user.token = token;
-      delete user.password;
-      const perm = {
-        orders: ["admin", "vendedor", "confeccionista", "almacenista"],
-        catalog: ["admin", "vendedor"],
-        clothes: ["admin", "confeccionista"],
-        materials: ["admin", "almacenista"],
-      }
-      res.status(200).json({ ...user, ...perm });
-    } else {
-      res.status(400).send("Invalid Credentials");
-    }
+    // const { dni, password } = req.body;
+    // if (!(dni && password)) {
+    //   res.status(400).send("All input is required");
+    // }
+    // let dbquery = `SELECT * FROM users WHERE dni=${dni}`;
+    // const oldUser = await db.query(dbquery);
+    // if (Array.isArray(oldUser) && oldUser.length === 0) {
+    //   return res.status(409).send("User Does not  Exist. Please Register");
+    // }
+    // const user = oldUser[0];
+    // if (await bcrypt.compare(password, user.password)) {
+    //   const token = jwt.sign(
+    //     { user_id: user.dni, dni },
+    //     process.env.TOKEN_KEY,
+    //     {
+    //       expiresIn: "2h",
+    //     }
+    //   );
+    //   user.token = token;
+    //   delete user.password;
+    //   const perm = {
+    //     orders: ["admin", "vendedor", "confeccionista", "almacenista"],
+    //     catalog: ["admin", "vendedor"],
+    //     clothes: ["admin", "confeccionista"],
+    //     materials: ["admin", "almacenista"],
+    //   }
+    //   res.status(200).json({ ...user, ...perm });
+    // } else {
+    //   res.status(400).send("Invalid Credentials");
+    // }
   } catch (err) {
     console.log(err);
   }
