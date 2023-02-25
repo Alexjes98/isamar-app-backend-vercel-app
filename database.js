881800bdd26bd2eea6056b10cbbb26727d5f148e
Alejandro Lopez <alexjes6543@gmail.com>
2023-02-25 19:49:36 -0400

    add login endpoint

--- database.js
+++ database.js
@@ -1,21 +1,23 @@
+const mysql = require("mysql2");
+const util = require("util");
+
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-  
-  const dbquery = util.promisify(connection.query).bind(connection);
-  
-  query = async (text) => {
-    try {
-      const rows = await dbquery(text);
-      return JSON.parse(JSON.stringify(rows));
-    } catch (e) {
-      console.log(e);
-      return [];
-    }
-  };
-  
-  module.exports = { query };
-  
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const dbquery = util.promisify(connection.query).bind(connection);
+
+query = async (text) => {
+  try {
+    const rows = await dbquery(text);
+    return JSON.parse(JSON.stringify(rows));
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
+
+module.exports = { query };
