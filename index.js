7a21a5da0be7c934673e168d08fa795912489008
Alejandro Lopez <alexjes6543@gmail.com>
2023-02-25 20:17:08 -0400

    add mysql

--- index.js
+++ index.js
@@ -2,11 +2,40 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const multer = require("multer");
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('./img'))
+
+const mysql = require("mysql2");
+const util = require("util");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const dbquery = util.promisify(connection.query).bind(connection);
+
+const query = async (text) => {
+  try {
+    const rows = await dbquery(text);
+    return JSON.parse(JSON.stringify(rows));
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
+
+const upload = multer({
+  limits: {
+    fileSize: 4 * 1024 * 1024,
+  }
+});
 
 app.get("/", (req, res) => {
   res.send(`API us up`);
@@ -21,5 +50,6 @@ app.get('/contact', (req, res) => res.send('Contact Page Route'));
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+
 
 module.exports = app;
